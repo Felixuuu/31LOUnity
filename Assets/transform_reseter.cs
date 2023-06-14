9c1c11a14340e65f70eb6452dd2c8b5d101366f5
Felixuuu <fpopielarz@interia.pl>
2023-06-14 16:42:29 +0200

    add column reset

--- Assets/transform_reseter.cs
+++ Assets/transform_reseter.cs
@@ -6,17 +6,29 @@ public class transform_reseter : MonoBehaviour
 {
     public Transform transformToReset;
     public Vector3 startingPosition;
-    public Vector3 startingRotation;
+    public Vector3 startingRotationAngles;
+    public Quaternion startingRotation;
     // Start is called before the first frame update
     void Start()
     {
+        if (transformToReset == null)
+        {
+            transformToReset = transform;
+        }
         startingPosition = transformToReset.position;
-        // startingRotation = transformToReset.rotation;
+        startingRotation = transformToReset.rotation;
+        startingRotationAngles = startingRotation.eulerAngles;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            transformToReset.position = startingPosition;
+            startingRotation.eulerAngles = startingRotationAngles;
+            transformToReset.rotation = startingRotation;
+        }
         
     }
 }
